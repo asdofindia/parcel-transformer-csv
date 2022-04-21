748bd99c29a5371442f8c8f875e4613f19727fff
Akshay S Dinesh <akshay@learnlearn.in>
2022-04-21 11:14:00 +0530

    feat: config support

--- README.md
+++ README.md
@@ -37,3 +37,15 @@ npm install --save-dev parcel-transformer-csv
   }
 }
 ```
+
+## Sample `.parcel-transformer-csv.json` config
+
+```
+{
+  "header": true
+}
+```
+
+The transformer takes an object as configuration. This object is the same as [PapaParse's config object](https://www.papaparse.com/docs#config). It is passed to `Papa.parse()` call as well.
+
+The above configuration, for example, tells Papa to treat the first row as headers and return an array of objects (with keys from header row) instead of array of arrays.
--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const { Transformer } = require("@parcel/plugin");
 const Papa = require("papaparse");
 
 module.exports = new Transformer({
-  async transform({ asset }) {
+  async loadConfig({ config }) {
+    let { contents, filePath } = await config.getConfig([
+      ".parcel-transformer-csv.json",
+    ]);
+    return JSON.parse(contents);
+  },
+  async transform({ asset, config }) {
     asset.type = "js";
     asset.setCode(
       `module.exports = JSON.parse(${JSON.stringify(
-        JSON.stringify(Papa.parse(await asset.getCode()))
+        JSON.stringify(Papa.parse(await asset.getCode(), config).data)
       )})`
     );
     return [asset];
